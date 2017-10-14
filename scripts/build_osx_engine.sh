c98af4204e31d33bb68c9689bc6cc393a0adc3bb
Alibek Omarov (a1batross) <a1ba.omarov@gmail.com>
2017-10-15 02:45:05 +0300

    travis: remove travis-upload script for osx, as it non portable(fix it!)Fix upload archive path

--- scripts/build_osx_engine.sh
+++ scripts/build_osx_engine.sh
@@ -8,4 +8,4 @@ CFLAGS="-m32" CXXFLAGS="-m32" cmake -DXASH_SDL=yes -DXASH_VGUI=yes -DXASH_DLL_LO
 make -j2
 cp engine/libxash.dylib mainui/libxashmenu.dylib vgui_support/libvgui_support.dylib ../vgui-dev/lib/vgui.dylib game_launch/xash3d ../xash3d.sh .
 cp ~/Library/Frameworks/SDL2.framework/SDL2 libSDL2.dylib
-tar -cjf xash3d-osx.tar.bz2 libxash.dylib libxashmenu.dylib libvgui_support.dylib xash3d libSDL2.dylib xash3d.sh
+tar -cjf $TRAVIS_BUILD_DIR/xash3d-osx.tar.bz2 libxash.dylib libxashmenu.dylib libvgui_support.dylib xash3d libSDL2.dylib xash3d.sh
